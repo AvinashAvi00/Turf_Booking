b22a7b7a8bec3904ce1deae87fea2612b47462c0
AvinashAvi00 <avinaskumaravi003@gmail.con>
2024-06-26 08:50:12 +0530

    Add backend and frontend directories

---
+++ frontend/src/Components/HomePage_Component/Description1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Description1(props) {
+    return (
+        <>
+        <div className="container  text-center bg-transparent "  >
+             
+            <h3>It's now easy to book turf. Sweat it out only on the field now.</h3> 
+        </div>
+        </>
+        );
+}
+export default Description1;
